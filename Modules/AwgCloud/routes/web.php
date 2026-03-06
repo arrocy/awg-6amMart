a72409e91f02597bd074e414718708ccd99b57ee
arrocy <94228760+arrocy@users.noreply.github.com>
2026-03-06 11:57:07 +0700

    Add admin middleware to AwgCloud admin routes
    To fix: ErrorException - Attempt to read property "role" on null.

--- Modules/AwgCloud/routes/web.php
+++ Modules/AwgCloud/routes/web.php
@@ -6,7 +6,8 @@ use Modules\AwgCloud\Http\Controllers\AwgCloudController;
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('awgclouds', AwgCloudController::class)->names('awgcloud');
 });
-Route::prefix('admin')->group(function () {
-    Route::get('/awgcloud', [AwgCloudController::class, 'index']) ->name('admin.awgcloud.index');
-    Route::post('/awgcloud', [AwgCloudController::class, 'update']) ->name('admin.awgcloud.update');
+
+Route::prefix('admin')->middleware('admin')->group(function () {
+    Route::get('/awgcloud', [AwgCloudController::class, 'index'])->name('admin.awgcloud.index');
+    Route::post('/awgcloud', [AwgCloudController::class, 'update'])->name('admin.awgcloud.update');
 });
